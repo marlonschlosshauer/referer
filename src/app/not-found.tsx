80f10dd788b09954d924b2e9a8441fcc477cc4cd
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2025-02-11 13:47:59 +0100

    Setup test

---
+++ src/app/not-found.tsx
@@ -0,0 +1,32 @@
+import { headers } from "next/headers";
+
+export default async function NotFound() {
+  const list = await headers();
+  const host = list.get("host");
+  const protocol = list.get("x-forwarded-proto");
+  const referer = list.get("referer");
+
+  if (!host || !referer || !protocol) {
+    return (
+      <div>
+        <h1>NotFound: No host, referrer or protocol found</h1>
+      </div>
+    );
+  }
+
+  const url = referer.slice(protocol.length + 3 + host.length, -1);
+
+  if (!url) {
+    return (
+      <div>
+        <h1>NotFound: No url found</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Url: {url}</h1>
+    </div>
+  );
+}
